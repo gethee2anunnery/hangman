f4f575976e58190b17a3997a09bb12291467ac26
Sara Perry <paraserry@gmail.com>
2014-12-07 17:44:18 -0500

    adding turn counter and MAX_TURNS

--- hangman.py
+++ hangman.py
@@ -7,22 +7,20 @@ def prompt_name():
 	name = raw_input('> ')
 	print "Welcome to hangman, %s! To get started, please choose a letter." % name
 
-def validate(letter):
+def validate_input(letter):
 	input_valid = len(letter) == 1 and letter.isalpha()
 	return input_valid
 
-
 def prompt_letter():
 	letter = raw_input('..> ')
-	is_valid = validate(letter)
+	is_valid = validate_input(letter)
 
 	if not is_valid:
 		print "That is not a valid input. Please try again."
 		letter = prompt_letter()
-		
 	else:
 		print "You chose '%s'!" % (letter)
-		
+
 	return letter
 
 
@@ -41,7 +39,6 @@ def populate_word(secret_word, populated_word, guess_list):
 	check_for_win(populated_word, secret_word)
 
 def play_again():
-
 	print "Play again? y/n"
 	answer = raw_input('>> ')
 	if answer == 'y':
@@ -67,7 +64,9 @@ def get_word():
 	word_list = sum([i for i in word_list],[]) #To flatten the list
 	return random.choice(word_list)
 
-def take_turn(secret_word, populated_word, guess_list):
+
+
+def take_turn(secret_word, populated_word, guess_list, turn_counter):
 	print "Your secret word -> %s" % populated_word
 	print "Your guesses so far -> %s" % guess_list
 
@@ -76,26 +75,28 @@ def take_turn(secret_word, populated_word, guess_list):
 	if chosen_letter:
 		if chosen_letter in guess_list:
 			print "You already guessed that letter! Give us a different one."
-			take_turn(secret_word,populated_word, guess_list)
+			take_turn(secret_word, populated_word, guess_list,turn_counter)
 		else:
+			turn_counter = turn_counter + 1
 			guess_list.append(chosen_letter)
 			if chosen_letter in list(secret_word):
 				print "You got one right!"
-				populate_word(secret_word,populated_word,guess_list)
+				populate_word(secret_word, populated_word, guess_list)
 
 			else:
 				print "No %s in that word. Guess again." % chosen_letter
 
-	take_turn(secret_word,populated_word, guess_list)
+	take_turn(secret_word, populated_word, guess_list, turn_counter)
 
 
 def start_game():
 	prompt_name()
 	guess_list = []
+	turn_counter = 0
 	secret_word = get_word()
 	populated_word = ['_' for letter in list(secret_word) ]
-	take_turn(secret_word, populated_word, guess_list)
+	take_turn(secret_word, populated_word, guess_list, turn_counter)
 
-
+MAX_TURNS = 10
 start_game()
 
